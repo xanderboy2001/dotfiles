174c2e2328c4887e12bc5551728dcd6225aec121
Alexander Christian <alexanderechristian@gmail.com>
2024-05-17 11:54:24 -0400

    Added checks to only run fehbg and nvidia commands if file/module exists

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -12,10 +12,10 @@ xscreensaver --no-splash &
 xautolock -time 30 -locker 'systemctl suspend' -notify 30 -notifier dunstify &
 
 # Set Wallpaper
-/home/alex/.fehbg &
+if [ -f /home/alex/.fehbg ]; then /home/alex/.fehbg & fi
 
 # Load ~/.nvidia-settings-rc
-nvidia-settings --load-config-only
+if ( lsmod | grep 'nvidia' ); then nvidia-settings --load-config-only & fi
 
 # Autostart apps
 bash "/home/alex/.config/qtile/AppStart.sh" &
