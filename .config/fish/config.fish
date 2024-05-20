56cc8a07e696ecf0b2498059201fe1b68890c699
Alexader Christian <alexanderechristian@gmail.com>
2024-05-20 16:50:26 -0400

    Reformatted comments. Added env vars to fix man output

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,27 +7,22 @@ set -U fish_user_paths $HOME/.bin  $HOME/.local/bin $HOME/Scripts /var/lib/flatp
 ### EXPORT ###
 set fish_greeting                                 # Supresses fish's intro message
 set TERM "xterm-256color"                         # Sets the terminal type
-set EDITOR "nvim"       		                  # $EDITOR use vi in terminal
-set VISUAL "nvim"		                          # $VISUAL use nvim
+set EDITOR "nvim"       		                  		# $EDITOR use vi in terminal
+set VISUAL "nvim"		                          		# $VISUAL use nvim
 set QT_STYLE_OVERRIDE "kvantum"
+set VIRTUAL_ENV_DISABLE_PROMPT "1"								# Disable venv text from prepending shell prompt
+set -x MANROFFOPT "-c"														# Disable color output in man pages as bat already does it
+set -x SHELL /bin/fish														# Make default shell fish
 
 ### SET MANPAGER
-### Uncomment only one of these!
-
 ### "bat" as manpager
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
-### "vim" as manpager
-# set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
-
-### "nvim" as manpager
-# set -x MANPAGER "nvim -c 'set ft=man' -"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"			# Set bat as the manpager
 
 ### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
-#function fish_user_key_bindings
+function fish_user_key_bindings
   # fish_default_key_bindings
-  # fish_vi_key_bindings
-  #end
+   fish_vi_key_bindings
+  end
 ### END OF VI MODE ###
 
 ### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
@@ -48,4 +43,4 @@ end
 starship init fish | source
 
 ### Initializing zoxide ###
-zoxide init fish --cmd cd | source
+zoxide init fish | source
