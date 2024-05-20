7d3848cdab46ca12386de9ec32407a1bed1fd725
Alexader Christian <alexanderechristian@gmail.com>
2024-05-20 12:01:40 -0400

    Ran shell formatter

--- Scripts/confirm.sh
+++ Scripts/confirm.sh
@@ -1,12 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 function confirm_yn {
-		case $1 in
-				""|[yY]*) return 0 ;;
-				[nN]*) return 1 ;;
-		esac
+	case $1 in
+	"" | [yY]*) return 0 ;;
+	[nN]*) return 1 ;;
+	esac
 }
-read -p "confirm (Y/n): " CONFIRM
-if ( confirm_yn $CONFIRM ); then
-		echo "You chose yes"
+read -rp "confirm (Y/n): " CONFIRM
+if (confirm_yn "$CONFIRM"); then
+	echo "You chose yes"
 else
-		echo "you chose no"
+	echo "you chose no"
 fi
