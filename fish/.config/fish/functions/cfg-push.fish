e3144acebb4322f5aa57cf7b2ad4b2a9954ad01c
Alexander Christian <alexanderechristian@gmail.com>
2026-01-27 08:51:21 -0500

    Added aliases to commit and push dotfile repo changes

---
+++ fish/.config/fish/functions/cfg-push.fish
@@ -0,0 +1,3 @@
+function cfg-push --wraps='pushd /home/alex/.dotfiles && git push; popd' --description 'alias cfg-push=pushd /home/alex/.dotfiles && git push; popd'
+    pushd /home/alex/.dotfiles && git push; popd $argv
+end
