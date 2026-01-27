aa6b146f30f125e317f93ed9ecec0b5a8a3a5be8
Alexander Christian <alexanderechristian@gmail.com>
2026-01-27 09:44:10 -0500

    Added bash skeleton

---
+++ nvim/.config/nvim/templates/bash_skeleton.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Strict settings
+set -o errexit
+set -o popefail
+set -o nounset
+
+# On-the-fly-debugging
+[[ -n "${DEBUG:-}" ]] && set -x
+
+# "Magic" variables
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename "${__file}" .sh)"
+
+# Borrowed from https://paul.af/bash-script-preamble-boilerplate
+
+
+
+
